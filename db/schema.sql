476279e024f0344d75c003fd0607ea94ab39a41e
Fancy Nancy <ramireznancy.686@gmail.com>
2023-09-06 22:52:16 -0600

    initial commit

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS department_db;
+CREATE DATABASE department_db;
+
+USE department_db;
+
+CREATE TABLE department(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30),
+);
+
+CREATE TABLE role(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL (10, 2),
+    department_id INT NULL,
+    FOREIGN KEY (department_id) 
+    REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    manager_id INT NULL,
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
+); 
